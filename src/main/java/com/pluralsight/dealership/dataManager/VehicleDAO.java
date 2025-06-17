25aeffa86b7547dbb562a119f9ef7573667eb3bb
Daomei Dong <daomeidong@Macmini.home>
2025-06-16 21:47:32 -0400

    formatted printing

--- src/main/java/com/pluralsight/dealership/dataManager/VehicleDAO.java
+++ src/main/java/com/pluralsight/dealership/dataManager/VehicleDAO.java
@@ -39,20 +39,37 @@ public class VehicleDAO {
     }
 
     public void getAllVehicles(){
-        ArrayList<Vehicle> results = new ArrayList<>();
-
         // Create the connection and prepared statement
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement =
                      connection.prepareStatement(
-                             "SELECT * FROM inventory;");
+                             """
+                        SELECT inventory.dealership_id, vehicles.VIN, vehicles.year, vehicles.make,
+                               vehicles.model, vehicles.type, vehicles.color,
+                               vehicles.mileage, vehicles.price, vehicles.SOLD
+                        FROM inventory
+                        LEFT JOIN vehicles ON inventory.VIN = vehicles.VIN;
+                     """);
         ) {
             try (ResultSet resultSet = preparedStatement.executeQuery()
             ) {
+                System.out.printf("%-5s %-18s %-6s %-10s %-15s %-10s %-10s %-8s %-10s %-5s%n",
+                        "ID", "VIN", "Year", "Make", "Model", "Type", "Color", "Miles", "Price", "Sold");
+
+                System.out.println("---------------------------------------------------------------------------------------------------------------");
+
                 while (resultSet.next()) {
-                    System.out.printf(
-                            "dealership_id = %d, VIN = %s",
-                            resultSet.getInt(1), resultSet.getString(2));
+                    System.out.printf("%-5d %-18s %-6d %-10s %-15s %-10s %-10s %-8d $%-9.2f %-5s%n",
+                            resultSet.getInt("dealership_id"),
+                            resultSet.getString("VIN"),
+                            resultSet.getInt("year"),
+                            resultSet.getString("make"),
+                            resultSet.getString("model"),
+                            resultSet.getString("type"),
+                            resultSet.getString("color"),
+                            resultSet.getInt("mileage"),
+                            resultSet.getBigDecimal("price"),
+                            resultSet.getBoolean("SOLD") ? "Yes" : "No");
                 }
             }
         }
